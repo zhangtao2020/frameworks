78aff85a6b106db70e4e2d3c03cad58316c97a5b
zt20192020 <278766377@qq.com>
2020-08-12 18:12:20 +0800

    添加参数过滤注解

--- base/src/main/java/com/tao/frameworks/base/common/BaseParam.java
+++ base/src/main/java/com/tao/frameworks/base/common/BaseParam.java
@@ -1,14 +1,15 @@
 package com.tao.frameworks.base.common;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class BaseParam {
 
+    @ApiModelProperty("当前页码")
     private int page = 1;
 
+    @ApiModelProperty("每页显示数量")
     private int limit = 10;
-
-    private String token;
 
 }
